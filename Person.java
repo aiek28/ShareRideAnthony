e36f844b8dee624431371086228aef2a7f9791a1
Anthony Iek <anthonyiek2010@gmail.com>
2026-02-17 15:10:18 -0800

    added arrived boolean

--- Person.java
+++ Person.java
@@ -1,7 +1,10 @@
 public class Person {
     int destination;
+    boolean arrived;
     
     public Person(int endDestination){
         destination = endDestination;
+        arrived = false;
     }
+
 }
