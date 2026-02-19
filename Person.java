ecc70b9c28fbdcca9e7bcd73dc76da0861b13c33
Anthony Iek <anthonyiek2010@gmail.com>
2026-02-19 09:55:58 -0800

    Made the person class (constructor) and toString

--- Person.java
+++ Person.java
@@ -1,10 +1,24 @@
 public class Person {
+    int start;
     int destination;
+    int idNo;
     boolean arrived;
+    private static int idGen = 1;
     
-    public Person(int endDestination){
-        destination = endDestination;
+    /**
+     * Create Person. Random values can be fed through the simulator.
+     * 
+     * @param myStart
+     * @param myDestination
+     */
+    public Person(int myStart, int myDestination){
+        start = myStart;
+        destination = myDestination;
+        idNo = idGen;
         arrived = false;
+        idGen++;
     }
-
+    public String toString(){
+        return "id Number: " + idNo + " Arrived: " + arrived;
+    }
 }
