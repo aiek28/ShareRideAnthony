64edd1145f75884c1dad8161a8c7154033297d3a
Anthony Iek <anthonyiek2010@gmail.com>
2026-02-20 12:25:31 -0800

    added dropPassengers

--- Station.java
+++ Station.java
@@ -15,5 +15,35 @@ public class Station {
         carWaiting = new ArrayList<Car>();
         carArrived = new ArrayList<Car>();
         int idNo = idGen;
+        idGen ++;
+    }
+    
+    public void dropPassengers(){
+        for (Car car: carWaiting){
+            ArrayList<Person> droppedPeople; // getting the list returned from dropPerson function in car class
+
+            ArrayList<Person> passengers = car.getPassengers(); // getting all the passengers in one car
+            for (Person person: passengers){ //getting all passengers in the car
+                if (person.getDestination() == idNo){ //seeing if their destination is equal to the station
+                    droppedPeople = car.dropPerson(person); //dropping person
+                    for (Person p: droppedPeople){
+                        peopleArrived.add(p);
+                    }
+                }
+                else if (car.getCurrStation() == car.getDestination()){ // if car has reached destination
+                    droppedPeople = car.dropPerson(person);
+                    for (Person p: droppedPeople){
+                        if (car.getCurrStation() == p.getDestination()){ // the 
+                            
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void takePassengers(){
+
     }
 }
