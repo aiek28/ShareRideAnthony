f3e3c440f7c610d98325fbb00a13b255b5ffff77
Anthony Iek <anthonyiek2010@gmail.com>
2026-02-24 13:47:36 -0800

    made populate car and populate people

--- Road.java
+++ Road.java
@@ -1,9 +1,49 @@
 import java.util.*;
 
 public class Road {
-    private ArrayList<Station> road;
+    private Station[] stations;
+    private int numStations;
+    
+    public void populateStations(int num){ // populate station with s
+        numStations = num;
+        for (int i=0; i < numStations; i++){
+            Station s = new Station();
+            stations[i] = s;
+        }
+    }
+    public void populateCars(int numCars){
+        for (int i = 0; i < numCars; i++){ // creating the each car
+            // creating start and end position of car
+            int startPos = (int)(Math.random() * numStations);
+            int endPos = (int)(Math.random() * numStations);
+            while (startPos == endPos){ // prevent start == end 
+                endPos = (int)(Math.random() * numStations);
+            }
 
-    public Road(){
-        
+            Car car = new Car(startPos, endPos);
+
+            int randNum = (int)(Math.random() * numStations); // creating random number to assign into the train
+            Station stationToAddCar = stations[randNum];
+
+            stationToAddCar.addCarsWaiting(car);
+        }
+    }
+
+    public void populatePeople(int numPeople){
+        for (int i = 0; i < numPeople; i++){ // creating the each car
+            // creating start and end position of person
+            int startPos = (int)(Math.random() * numStations);
+            int endPos = (int)(Math.random() * numStations);
+            while (startPos == endPos){ // prevent start == end 
+                endPos = (int)(Math.random() * numStations);
+            }
+
+            Person person = new Person(startPos, endPos);
+
+            int randNum = (int)(Math.random() * numStations); // creating random number to assign into the train
+            Station stationToAddCar = stations[randNum];
+
+            stationToAddCar.addPassengerWaiting(person); // 
+        }
     }
 }
