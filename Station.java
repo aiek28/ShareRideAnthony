6793adc27b2bd5fe885859a92d1f03c4c09feeb8
Anthony Iek <anthonyiek2010@gmail.com>
2026-03-08 18:55:05 -0700

    got rid of most of the station logic. It was too complicated so i shifted all the main logic to the road. I also updated the toString and fixed minor issues

--- Station.java
+++ Station.java
@@ -4,55 +4,52 @@ public class Station {
     // fields
     private ArrayList<Person> peopleWaiting;
     private ArrayList<Person> peopleArrived;
-    private ArrayList<Car> carWaiting;
-    private ArrayList<Car> carArrived;
     private int idNo;
     private static int idGen = 0;
 
     public Station() {
         peopleWaiting = new ArrayList<Person>();
         peopleArrived = new ArrayList<Person>();
-        carWaiting = new ArrayList<Car>();
-        carArrived = new ArrayList<Car>();
-        int idNo = idGen;
+        idNo = idGen;
         idGen++;
     }
 
-    public void dropPassengers() {
-        for (Car car : carWaiting) {
-            ArrayList<Person> passengers = car.getPassengers(); // getting all the passengers in one car
-            for (Person p : passengers) { // getting all passengers in the car
-                if (p.getDestination() == idNo) { // seeing if their destination is equal to the station
-                    peopleArrived.add(car.dropPerson(p)); //dropping person from car and adding it to station
-                } else if (car.getCurrStation() == car.getDestination()) { // if car has reached destination
-                    for (Person pWait: passengers){
-                        carArrived.add(car);
-                        peopleWaiting.add(car.dropPerson(pWait));
-                    }
-                }
-            }
-        }
-
+    public int getID(){
+        return idNo;
     }
 
-    public void takePassengers() {
-        for (Car car: carWaiting){
-            Iterator<Person> it = peopleWaiting.iterator();
-            while (it.hasNext()){ // allows us to loop through peopleWaiting while also deleting them once they board a car
-                Person p = it.next();
-                if (car.hasSpace() && (p.getDirection() == car.getDirection())){ // if has space and going in the same direction
-                    it.remove();
-                    car.addPerson(p);
-                }
-            }
-        }
+    public ArrayList<Person> getPeopleWaiting(){
+        return peopleWaiting;
     }
 
-    public void addCarsWaiting(Car car){ // adds cars when you build the station
-        carWaiting.add(car);
+    public ArrayList<Person> getPeopleArrived(){
+        return peopleArrived;
     }
 
     public void addPassengerWaiting(Person person){
         peopleWaiting.add(person);
     }
+
+    public void addPassengerArrived(Person person){
+        peopleArrived.add(person);
+    }
+
+    public String toString(){
+        String s = "Station ID: " + idNo + "\n";
+
+        s += "Current People Waiting: ";
+        for (Person waiting: peopleWaiting){
+            s += "(" + waiting.toString() + ")"; // calling toString on person
+        }
+        s += "\n";
+
+        s += "Current People Arrived: ";
+        for (Person arrived: peopleArrived){
+            s += "(" + arrived.toString() + ")";
+        }
+        s += "\n";
+        
+        return s;
+    }
+
 }
