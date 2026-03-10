e1f28927c4eeec8bd39124d02f01243dc2c044c5
Anthony Iek <anthonyiek2010@gmail.com>
2026-03-10 07:43:31 -0700

    Macde readme and adjusted tester to the format requried for submitting

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # ShareRideAnthony
+
+## Project Description
+- Person 
+    - Has all the initial information about a passenger (start + end destination, idNo, direction, etc)
+    - Connects to the Car class
+- Car
+    - Has an ArrayList of passengers
+    - Has simple functions including add and drop passenger
+- Station
+    - Has ArrayList of passengers arrived and waiting
+    - Decided not to let the station keep track of car
+- Road
+    - Contains all the logic
+        - Populates the station, car, and people
+        - runRoad()
+            - Contains helper methods 
+                - Pick up and drop off potential passengers
+                - Drop off all if car is full
+
+## Simulation Results after 5,000 simulations:
+20 cars: 57.544% of people make it to their final destination
+
+40 cars: 79.6812% of people make it to their final destination
+
+## Proud & struggles
+I am proud of the logic of the entire program. I made it a lot more cleaner towards the end. 
+I like my change of making all the other classes like Car, Person, and Station very simple while putting all the main logic on to the road. 
+
+I struggled with having logic of having all the files connect with each other. Initially, my station was too complicated (accomplishing lots of what the road was doing), so it made it difficult to combine all the components together. 
--- Tester.java
+++ Tester.java
@@ -1,32 +1,48 @@
 public class Tester {
     public static void main (String[] args){
-        double totalPercent = 0.0;
-        int trials = 5000;
+        // double totalPercent = 0.0;
+        // int trials = 5000;
 
-        for (int i = 1; i <= trials; i++){
-            Person.resetIDGen();
-            Car.resetIDGen();
-            Station.resetIDGen();
-
-            Road road = new Road();
+        // running regular simulation without testing out number of trials
+        Road road = new Road();
 
             road.populateStations(32);
             road.populateCars(20);
             road.populatePeople(50);
 
-            // System.out.println("BEFORE RUNNING: ");
-            // System.out.println(road);
+            System.out.println("BEFORE RUNNING: ");
+            System.out.println(road);
 
             road.runRoad();
-            // System.out.println("AFTER RUNNING: ");
-            // System.out.println(road);
-            
-            double percent = road.getPercentArrived();
-            totalPercent += percent;
+            System.out.println("AFTER RUNNING: ");
+            System.out.println(road);
 
-            System.out.println("Trial " + i + ": " + percent + "%");
-        }
-        System.out.println();
-        System.out.println("Average Percent Arrived: " + (totalPercent/trials));
+
+        // This is for the simulation
+        // for (int i = 1; i <= trials; i++){
+        //     Person.resetIDGen();
+        //     Car.resetIDGen();
+        //     Station.resetIDGen();
+
+        //     Road road = new Road();
+
+        //     road.populateStations(32);
+        //     road.populateCars(20);
+        //     road.populatePeople(50);
+
+        //     // System.out.println("BEFORE RUNNING: ");
+        //     // System.out.println(road);
+
+        //     road.runRoad();
+        //     // System.out.println("AFTER RUNNING: ");
+        //     // System.out.println(road);
+            
+        //     double percent = road.getPercentArrived();
+        //     totalPercent += percent;
+
+        //     System.out.println("Trial " + i + ": " + percent + "%");
+        // }
+        // System.out.println();
+        // System.out.println("Average Percent Arrived: " + (totalPercent/trials));
     }
 }
